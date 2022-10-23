190b109f079aacbb0657647cd854877df45c0e38
Hu Kaixiang <maxhu787@gmail.com>
2022-10-23 12:18:48 +0800

    e

--- pdo.php
+++ pdo.php
@@ -1,28 +1,25 @@
 <?php
-        $HOST = '85.10.205.173';
-    $PORT = 3306;
-    $DB_NAME = 'g4o2_chat';
-    $DB_USER = 'maxhu787';
-    $DB_PASSWORD = getenv('DB_PASSWORD');
-    $pdo = new PDO(
-        "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-/*
-<?php
+/*  
+$HOST = '85.10.205.173';
+$PORT = 3306;
+$DB_NAME = 'g4o2_chat';
+$DB_USER = 'maxhu787';
+$DB_PASSWORD = getenv('DB_PASSWORD');
+$pdo = new PDO(
+    "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+*/
     
 
 
 ob_start();
-    $HOST = 'localhost';
-    $PORT = 3306;
-    $DB_NAME = 'misc';
-    $DB_USER = 'g4o2';
-    $DB_PASSWORD = 'g4o2';
-    $pdo = new PDO(
-        "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-*/
+$HOST = 'localhost';
+$PORT = 3306;
+$DB_NAME = 'misc';
+$DB_USER = 'g4o2';
+$DB_PASSWORD = 'g4o2';
+$pdo = new PDO(
+    "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
