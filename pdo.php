57da61135232a461b507d3d1f1686bc057174acb
Hu Kaixiang <maxhu787@gmail.com>
2022-11-06 22:30:01 +0800

    update

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
 
-/*
+
 $HOST = 'sql12.freemysqlhosting.net';
 $PORT = 3306;
 $DB_NAME = 'sql12537346';
@@ -11,14 +11,16 @@ $pdo = new PDO(
     "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-*/
 
+/*
 $HOST = 'localhost';
 $PORT = 3306;
 $DB_NAME = 'misc';
 $DB_USER = 'g4o2';
 $DB_PASSWORD = 'g4o2';
 $pdo = new PDO(
-    "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD
+    "mysql:host=$HOST;port=$PORT;dbname=$
+    DB_NAME", $DB_USER, $DB_PASSWORD
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+*/
