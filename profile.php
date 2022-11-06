5e4ac65215954a5c489d03434c28211f404ad351
Hu Kaixiang <maxhu787@gmail.com>
2022-11-07 00:22:48 +0800

    update

--- profile.php
+++ profile.php
@@ -9,33 +9,9 @@ if(!isset($_GET['user'])) {
     die();
 }
 $pfpsrc = './default-pfp.png';
-
 $stmt = $pdo->prepare("SELECT * FROM account WHERE user_id=?");
 $stmt->execute([$_GET['user']]);
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-if (count($rows) > 0) {
-    foreach ($rows as $test) {
-        if ($test['pfp'] != null) {
-            $pfpsrc = $test['pfp'];
-        }
-        $show_email = $test['show_email'];
-        $user = "<div id='user-name'><p>{$test['name']}</p></div>";
-        $pfp = "<img id='profile-image' src='$pfpsrc'>";
-        $email = "<div id='user-email'><p>{$test['email']}</p></div>";
-        $about = "<div id='user-about'><p>{$test['about']}</p></div>";
-    }
-    echo $pfp;
-    echo $user;
-    echo $about;
-    if ($show_email === "True") {
-        echo $email;
-    } else {
-        echo "";
-    }
-} else {
-    echo "<p style='font-size: 22px;font-family: Arial;text-align:center;color:red;'>User not found</p>";
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,46 +20,35 @@ if (count($rows) > 0) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./style.css?v=<?php echo time(); ?>">
-    <style>
-        body {
-            text-align: center;
-            padding: 30px;
-            background-image: none;
-            background-color: rgb(41, 41, 41);
-        }
-
-        #user-name {
-            font-size: 40px;
-            font-family: 'orbitron';
-        }
-
-        #user-email {
-            font-size: 18px;
-            font-family: Arial, Helvetica, sans-serif;
-            background-color: red;
-            padding: 4px 0px 4px 0px;
-            width: 28%;
-            margin: 0 auto;
-            border-radius: 8px;
-        }
-
-        #user-about {
-            color: red;
-            font-family: monospace;
-            font-size: 20px;
-        }
-        #profile-image {
-            height: 200px;
-            border-radius: 10px;
-            border: solid 5px #ffa500;
-        }
-    </style>
+    <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="./css/profile.css">
 </head>
 
 <body>
-
+    <?php
+        if (count($rows) > 0) {
+            foreach ($rows as $test) {
+                if ($test['pfp'] != null) {
+                    $pfpsrc = $test['pfp'];
+                }
+                $show_email = $test['show_email'];
+                $user = "<div id='user-name'><p>{$test['name']}</p></div>";
+                $pfp = "<img id='profile-image' src='$pfpsrc'>";
+                $about = "<div id='user-about'><p>{$test['about']}</p></div>";
+                if ($show_email === "True") {
+                    $email = "<div id='user-email'><p>{$test['email']}</p></div>";
+                } else {
+                    $email = "Hidden";
+                }
+            }
+            echo $pfp;
+            echo $user;
+            echo $about;
+            echo $email;
+        } else {
+            echo "<p style='font-size: 22px;font-family: Arial;text-align:center;color:red;'>User not found</p>";
+        }
+    ?>
 </body>
 
 </html>
