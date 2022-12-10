7ea195f392748dfa1ca36c62d40787f8b9b26908
Hu Kaixiang <maxhu787@gmail.com>
2022-12-11 01:08:20 +0800

    /

--- head.php
+++ head.php
@@ -3,9 +3,9 @@
 <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-<meta name="description" content="">
+<meta name="description" content="A chat application made with PHP and SQL">
 <meta name="keywords" content="">
-<meta name="author" content="">
+<meta name="author" content="Hu Kaixiang">
 
 <meta name="robots" content="index, follow">
 <meta name="mobile-web-app-capable" content="yes">
@@ -19,25 +19,25 @@
 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
 <meta name="msapplication-TileColor" content="#da532c">
 <meta name="theme-color" content="#ffffff">
 
 <meta property="fb:page_id" content="">
-<meta property="og:title" content="">
-<meta property="og:image" content="">
-<meta property="og:description" content="">
-<meta property="og:url" content="">
-<meta property="og:site_name" content="">
+<meta property="og:title" content="g4o2-chat">
+<meta property="og:image" content="https://user-images.githubusercontent.com/103299803/196030342-1b944181-2ba5-4c1b-b762-c6e6827cc5cd.PNG">
+<meta property="og:description" content="A chat application made with PHP and SQL">
+<meta property="og:url" content="https://php-sql-chat.maxhu787.repl.co">
+<meta property="og:site_name" content="g4o2-chat">
 <meta property="og:type" content="website">
 
 <meta name="twitter:card" content="summary">
-<meta name="twitter:url" content="">
-<meta name="twitter:title" content="">
-<meta name="twitter:description" content="">
-<meta name="twitter:image" content="">
-<meta name="twitter:site" content="">
+<meta name="twitter:title" content="g4o2-chat">
+<meta name="twitter:image" content="https://user-images.githubusercontent.com/103299803/196030342-1b944181-2ba5-4c1b-b762-c6e6827cc5cd.PNG">
+<meta name="twitter:description" content="A chat application made with PHP and SQL">
+<meta name="twitter:url" content="https://php-sql-chat.maxhu787.repl.co/">
+<meta name="twitter:site" content="https://php-sql-chat.maxhu787.repl.co/">
 
 <script type="text/javascript" src="./js/jquery-3.6.0.js"></script>
 <script src="./particles/particles.js"></script>
