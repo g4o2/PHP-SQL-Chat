3f307236458bdd0af667760f69da061e68d159d3
Hu Kaixiang <maxhu787@gmail.com>
2022-12-17 12:02:58 +0800

    w

--- track_viewers.php
+++ track_viewers.php
@@ -3,4 +3,13 @@ $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 if($ip !== '216.144.248.19') {
     error_log("Page viewer " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/track_viewers.log");
 }
+/*
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+if($ip !== '216.144.248.19') {
+    if (strpos(file_get_contents("../logs/track_viewers.log"), $ip) !== false) {
+        error_log("Page viewer " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/track_viewers.log");
+    } else {
+
+    }
+}*/
 ?>
