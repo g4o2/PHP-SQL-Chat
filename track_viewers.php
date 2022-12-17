be6715fb0b4ca0b08ad0c5a3bd210f4eaa396c18
Hu Kaixiang <maxhu787@gmail.com>
2022-12-17 11:45:58 +0800

    viewer track update

--- track_viewers.php
+++ track_viewers.php
@@ -1,4 +1,6 @@
 <?php
 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-error_log("Page viewer " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/track_viewers.log");
+if($ip !== '216.144.248.19') {
+    error_log("Page viewer " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/track_viewers.log");
+}
 ?>
